da957a09a622f91a96625bccfea2b98b91ff6614
UnDev <dbsalsgur34@naver.com>
2017-10-30 09:08:39 -0700

    Auto stash before merge of "master" and "origin/master"
    draw points

--- run1.py
+++ run1.py
@@ -1,22 +1,93 @@
-from PyQt5.QtWidgets import *
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
+import random
 
 
-class myWidget(QDialog):
+
+
+
+class App(QMainWindow):
     def __init__(self):
-        QDialog.__init__(self)
+        super().__init__()
+        self.title = 'PyQt paint - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 440
+        self.height = 280
+        self.initUI()
 
-        # 레이블,Edit,버튼 컨트롤
-        label1 = QLabel("images.jpeg")
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # 레이아웃
-        layout = QVBoxLayout()
-        layout.addWidget(label1)
+        # Set window background color
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.black)
+        self.setPalette(p)
 
-        # 다이얼로그에 레이아웃 지정
-        self.setLayout(layout)
+
+        # Add paint widget and paint
+        self.m = PaintWidget(self)
+        self.m.move(0, 0)
+        self.m.resize(self.width, self.height)
+        for i in range(1, 1024):
+            x = random.randint(1, self.width-1)
+            y = random.randint(1, self.height-1)
+            self.m.appendData(x, y)
+        self.show()
+
+    def drawPoints(self):
+        for i in range(1, 2):
+            x = random.randint(1, self.width-1)
+            y = random.randint(1, self.height-1)
+            self.m.appendData(x, y)
 
 
-app = QApplication([])
-dialog = myWidget()
-dialog.show()
-app.exec_()
+class dataset():
+    def __init__(self):
+        self.color = Qt.white
+        self.list = []
+
+    def setColor(self, color):
+        self.color = color
+
+    def append(self, x, y):
+        self.list.append(x, y)
+
+
+class PaintWidget(QWidget):
+    datalist = []
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        qp.setPen(self.color)
+
+        for i in range(1, self.datalist.__len__()-500):
+            x = self.datalist[i][0]
+            y = self.datalist[i][1]
+            qp.drawPoint(x, y)
+
+        qp.setPen(Qt.red)
+
+        for i in range(self.datalist.__len__()-499, self.datalist.__len__()):
+            x = self.datalist[i][0]
+            y = self.datalist[i][1]
+            qp.drawPoint(x, y)
+
+    def appendData(self, x, y):
+        self.datalist.append([x, y])
+
+    def setColor(self, color):
+        self.color = color
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.m.setColor(Qt.blue)
+    ex.drawPoints()
+    print(ex.m.datalist.__len__())
+    sys.exit(app.exec_())
